bfffcaab0f36edd38dfc968448ef697c3e37a83c
Jason Breedlove <jasonbreedlove@icloud.com>
2025-07-23 11:41:36 -0700

    first beta merge test run

--- yet-another-media-player.js
+++ yet-another-media-player.js
@@ -2328,6 +2328,7 @@ function renderSearchSheet(_ref) {
   `;
 }
 
+// import { LitElement, html, css, nothing } from "https://unpkg.com/lit-element@3.3.3/lit-element.js?module";
 const SUPPORT_PREVIOUS_TRACK = 16;
 const SUPPORT_NEXT_TRACK = 32;
 const SUPPORT_TURN_ON = 128;
