51e086dfa32fb35e966acfc8a84f7ccb31c423d0
Daniel <80019353+thedirtyd@users.noreply.github.com>
2025-07-24 14:04:26 -0700

    Separate .js files for editor and shared constants (#31)
    * initial commit of config UI overhaul
    
    * hold_to_pin default value typo
    
    * fix domain filter on volume_entity entity picker
    
    * filter idle_image entity picker by camera, image
    
    * hide/reset sync_power if no volume_entity
    
    * use asterisk to denote required for entities group
    
    * seperate files for editor and constants
    
    * removed 'never' from show chip row dropdown

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// Supported feature flags
+export const SUPPORT_PAUSE = 1;
+export const SUPPORT_SEEK = 2;
+export const SUPPORT_VOLUME_SET = 4;
+export const SUPPORT_VOLUME_MUTE = 8;
+export const SUPPORT_PREVIOUS_TRACK = 16;
+export const SUPPORT_NEXT_TRACK = 32;
+export const SUPPORT_TURN_ON = 128;
+export const SUPPORT_TURN_OFF = 256;
+export const SUPPORT_PLAY_MEDIA = 512;
+export const SUPPORT_STOP = 4096;
+export const SUPPORT_PLAY = 16384;
+export const SUPPORT_SHUFFLE = 32768;
+export const SUPPORT_GROUPING = 524288;  
+export const SUPPORT_REPEAT_SET = 262144;
