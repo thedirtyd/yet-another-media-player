94f2a04e3103e938f1c5865df10cb915cae5e1bd
jianyu-li <52750081+jianyu-li@users.noreply.github.com>
2025-07-28 15:17:14 -0700

    Update progress-bar.js

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -1,4 +1,5 @@
-import { LitElement, html, css, nothing } from "lit";
+import { html, nothing } from "https://unpkg.com/lit-element@3.3.3/lit-element.js?module";
+// import { LitElement, html, css, nothing } from "lit";
 
 export function renderProgressBar({
   progress,
